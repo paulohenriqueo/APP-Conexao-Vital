92aca35667cfc9dec839edb4cb4b37b58cf88b3f
paulohenriqueo <paulohenrique300305@gmail.com>
2025-09-12 08:49:53 -0300

    :tada: init: first commmit in delevop

--- src/screens/auth/Login.tsx
+++ src/screens/auth/Login.tsx
@@ -18,6 +18,8 @@ export default function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    //first commit in development branch
+
     const handleLogin = () => {
         try {
             if (!email || !password) {
